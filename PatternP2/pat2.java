e0840c6cbe500395ccd2623f5d4e93d3d11035d3
rohit chauhan <rk9331498@gmail.com>
2026-08-26 17:37:03 +0530

    Inverted & Rotated Half Pyramid

--- PatternP2/pat2.java
+++ PatternP2/pat2.java
@@ -30,7 +30,28 @@ public class Pat2{
 
 //Inverted & rotated half pyramid
 /*
-    *
-   **
+    * row = 1; space = 3; star =1;
+   ** row = 2 space = 2; star = 2
   ***
 *****
+logic
+space = n-1
+*/
+
+public class Pat2{
+    public static void inverted_hPyramid(int tRows){
+        for (int i = 1; i <= tRows; i++){
+            //space
+            for(int j = 1; j <= tRows - i;j++){
+                System.out.print(" ");
+            }
+            for (int j = 1 ; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    public static void main(String[] args){
+       inverted_hPyramid(10);
+    }
+}
