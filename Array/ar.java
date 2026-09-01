245a0428ed2a329f36bed30998d3c7afad537799
rohit chauhan <rk9331498@gmail.com>
2026-09-01 15:38:14 +0530

    Linear search

--- Array/ar.java
+++ Array/ar.java
@@ -19,7 +19,8 @@ System.out.print(marks[0]);
 */
 //(2)Direct initialization 
 //dataType arrayName = {value,value};
-
+//all sum
+/*
 public class ar{
     public static void main(String[] args){
        
@@ -33,5 +34,78 @@ public class ar{
          
 
 
+    }
+}
+*/
+
+//Array as function argument
+//Array should be pass by refrence and pass by refrence value can change in main function 
+//Array refrence main function se upr me 
+/*
+public class ar{
+    public static void updateArr(int marks[]){
+        for(int i=0; i<marks.length; i++){
+            marks[i] = marks[i] + 1;
+        }
+    }
+    public static void main(String[] arg){
+        int marks[] = {99,199,129,99};
+        updateArr(marks);
+        for (int i = 0; i<marks.length; i++){
+                        System.out.println(marks[i]);
+        }
+        
+    }
+}
+*/
+
+//first algorithem - linear search
+//finding the value one by one the given value/key
+/*
+public class ar{
+    public static int linearSearch(int marks[],int key){
+        for (int i = 0; i < marks.length; i++){
+            if(marks[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] arg){
+        int marks[] = {2,3,4,5,6,123,34,5};
+        int key = 12;
+        
+        int index = linearSearch(marks,key);
+        if(index == -1){
+            System.out.print("Not found");
+        }
+        else{
+            System.out.print("Key is on index = " + index);
+        }
+    }
+}
+*/
+
+//Find the largest number in given arra
+import java.util.*;
+public class ar{
+    public static int largestNum(int number[]){//argument function -send value to the parameter
+        int largest = Integer.MIN_VALUE;//-INFINIT
+        int smallest = Integer.MAX_VALUE;//INFINITY
+        for(int i = 0; i <number.length; i++){
+            if(largest < number[i]){
+                largest = number[i];
+            }
+            if(smallest > number[i]){
+                smallest = number[i];
+            } 
+        }
+        System.out.println("Smallest num in the given array = " + smallest);
+        return largest;
+    }
+    public static void main(String[] arg){
+        int number[] = {2,4,5,6,7,2,10,3};
+       
+        System.out.print("Largest num is = " + largestNum(number));//parameter function
     }
 }
