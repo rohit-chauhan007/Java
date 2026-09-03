fd58926ebe74926710449d2fdfc646f9bd322f36
rohit chauhan <rk9331498@gmail.com>
2026-09-03 12:20:22 +0530

    smallest number find

--- Array/linear.java
+++ Array/linear.java
@@ -3,6 +3,7 @@
 //find the index of given array 
 //[2,1,10,3] target = 10;
 
+/*
 public class linear{
     public static  int linearSearch(int number[],int target ){
         for(int i = 0; i < number.length; i++){
@@ -22,4 +23,43 @@ public class linear{
             System.out.print("The num is in index = " + index);
         }
     }
+}
+*/
+/*
+//find the greatest num in given array
+import java.util.*;
+public class linear{
+   public static int largestNum(int num[]){
+    int largest = Integer.MIN_VALUE;
+    for(int i = 0; i < num.length; i++){
+        if(largest < num[i]){
+            largest = num[i];
+        }
+    }
+    return largest;
+   }
+   public static void main(String[] arg){
+    int num[] = {3,43,5,6,7,454,54};
+   
+    System.out.print( largestNum(num));
+   }
+}
+*/
+//smallest number 
+import java.util.*;
+public class linear{
+    public static int smallestNum(int num[]){
+        int smallest = Integer.MAX_VALUE;
+        for(int i = 0; i < num.length; i++){
+            if(smallest>num[i]){
+                smallest = num[i];
+            }
+        }
+        return smallest;
+    }
+   public static void main(String[] arg){
+        int num[] = {8,5,43,5,6,3};
+        System.out.print(smallestNum(num));
+   }
+  
 }
