b63695d57bd4a9181978328fa2a97eaced774142
rohit chauhan <rk9331498@gmail.com>
2026-09-13 16:51:26 +0530

    Kadanes Algorithem

--- Array/subArr.java
+++ Array/subArr.java
@@ -19,6 +19,7 @@
 // }
 
 //max subArray sum 
+/*
 import java.util.*;
 public class subArr{
     public static void sumSub(int num[]){
@@ -43,5 +44,67 @@ public class subArr{
         int num[] = {2,3,4};
         sumSub(num);
        
+    }
+}
+*/
+/*
+Prefix sum
+import java.util.*;
+public class subArr{
+    public static void prefixSum(int num[]){
+        int maxSum = Integer.MAX_VALUE;
+        int currSum = 0;
+        //prefix array 
+        int prefix[] = new int[num.length];
+        prefix[0] = num[0];
+
+        //calculate sum and store in prefix arr
+        for (int i = 1; i<prefix.length;i++){
+            prefix[i] = prefix[i-1] + prefix[i];//previus sum  + new sum;
+        }
+       for (int i = 0; i<num.length;i++){
+        for(int j = i; j<num.length; j++){
+            for(int k = i; k<=j;k++){
+                currSum = i == 0 ? prefix[j] : prefix[j] - prefix[i-1];
+                if(maxSum < currSum){
+                        maxSum = currSum;
+                    }  
+            }
+
+        }
+          
+       }
+       System.out.print(maxSum);
+      
+    }
+    public static void main(String[] arg){
+        int num[] = {2,3,4};
+        prefixSum(num);
+    }
+}
+/
+*/
+
+//KEDANES ALGORITHEM
+import java.util.*;
+public class subArr{
+    public static void kadanes(int number[]){
+        int ms = Integer.MIN_VALUE;
+        int cs = 0;
+        for(int i = 0; i< number.length; i++){
+             cs = cs + number[i];
+
+             ms = Math.max(cs,ms);
+           
+            if(cs<0){
+                cs = 0;
+            }
+           
+        }
+        System.out.print("Max sum = " + ms);
+    }
+    public static void main(String[] arg){
+        int number[] = {-2,-3,4,-1,-2,1,5,-3};
+        kadanes(number);
     }
 }
